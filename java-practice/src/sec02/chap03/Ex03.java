75491506ba750d6c3f034140e3e57997f3dcf578
seunghaneee <seunghan4769@pukyong.ac.kr>
2023-08-19 23:29:49 +0900

    java chap02 example

---
+++ java-practice/src/sec02/chap03/Ex03.java
@@ -0,0 +1,15 @@
+package sec02.chap03;
+
+public class Ex03 {
+    public static void main(String[] args) {
+        String 대학 = "학생";
+        System.out.println(대학);
+
+        //  일반적으로는 아래와 같이 선언과 초기화를 동시에
+        int age = 20;
+        boolean isMarried = false;
+        double height = 179.99;
+        char sex = 'M';
+        String name = "홍길동";
+    }
+}
