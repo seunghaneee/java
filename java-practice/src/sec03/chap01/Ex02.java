d91ca2946decbe4e13590bed7654b1c02f737f75
seunghaneee <seunghan4769@pukyong.ac.kr>
2023-08-20 10:24:40 +0900

    0820/10:24

--- java-practice/src/sec03/chap01/Ex02.java
+++ java-practice/src/sec03/chap01/Ex02.java
@@ -1,4 +1,15 @@
 package sec03.chap01;
 
 public class Ex02 {
+    public static void main(String[] args) {
+        byte byteNum;
+//        int smallIntNum = 123;
+        int smallIntNum = 12345;
+
+        //  명시적(강제) 형변환
+        //  - 개발자 : "내가 책임질테니까 그냥 넣으세요."
+        byteNum = (byte) smallIntNum; // 명시적 형변환
+
+        byte byteNum2 = 12345 % 128;
+    }
 }
--- java-practice/src/sec03/chap01/Ex03.java
+++ java-practice/src/sec03/chap01/Ex03.java
@@ -2,6 +2,18 @@ package sec03.chap01;
 
 public class Ex03 {
     public static void main(String[] args) {
+        // 이항 연산자 -> 결과값을 반환함
+        int a = 1 + 2;
+        int b = a - 1;
+        int c = b * a;
+        int d = a + b * c / 3;
+        int e = (a + b) * c / 3;
+        int f = e % 4;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
 
     }
 }
